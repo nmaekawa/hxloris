445a9581044481fce74486128b712f96b50af3fc
Arthur Barrett <abarrett@fas.harvard.edu>
2021-08-26 12:33:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.rst") as readme_file:
 
 requirements = [
     "botocore<=1.18.18",
-    "boto3>=1.15.18",
+    "boto3~=1.15.18",
     # hxloris depends on https://github.com/loris-imageserver/loris.git
     # but setup.py not always can install pkgs from git repos...
     #
